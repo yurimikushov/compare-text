2a8c752d5e464bb16900be3a9bd51c59128eafd4
Yuri Mikushov <mikushov.yu@gmail.com>
2022-01-17 20:21:34 +0300

    feat: add `<Layout />`

---
+++ src/layouts/Layout/Layout.jsx
@@ -0,0 +1,14 @@
+import cn from 'classnames'
+
+const Layout = ({ className, children, ...props }) => {
+  return (
+    <div
+      {...props}
+      className={cn(className, 'p-10 bg-gradient-to-br from-blue-50 to-white')}
+    >
+      <div className='mx-auto max-w-screen-xl w-full h-full'>{children}</div>
+    </div>
+  )
+}
+
+export default Layout
