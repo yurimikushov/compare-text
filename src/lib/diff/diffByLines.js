eee722f5588193ec0dd9a1a70c4593f42d4502e8
Yuri Mikushov <mikushov.yu@gmail.com>
2022-01-18 18:24:59 +0300

    refactor: extract util funcs

--- src/lib/diff/diffByLines.js
+++ src/lib/diff/diffByLines.js
@@ -1,12 +1,16 @@
 import diff from './diff'
 
+const tokenize = (text) => {
+  return text.split('\n')
+}
+
+const addLineBreak = ({ value, ...rest }) => ({
+  ...rest,
+  value: value + '\n',
+})
+
 const diffByLines = (text1, text2) => {
-  return diff(text1.split('\n'), text2.split('\n')).map(
-    ({ value, ...rest }) => ({
-      ...rest,
-      value: value + '\n',
-    })
-  )
+  return diff(tokenize(text1), tokenize(text2)).map(addLineBreak)
 }
 
 export default diffByLines
