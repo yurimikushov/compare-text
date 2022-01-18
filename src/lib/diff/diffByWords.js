856ea839af756b9823e8ef562ba669d86fe71a44
Yuri Mikushov <mikushov.yu@gmail.com>
2022-01-18 20:35:51 +0300

    fix: add white spaces afrer diffing

--- src/lib/diff/diffByWords.js
+++ src/lib/diff/diffByWords.js
@@ -4,8 +4,13 @@ const tokenize = (text) => {
   return text.replaceAll('\n', '\n ').split(' ')
 }
 
+const addWhiteSpace = ({ value, ...rest }) => ({
+  ...rest,
+  value: value.endsWith('\n') ? value : value + ' ',
+})
+
 const diffByWords = (text1, text2) => {
-  return diff(tokenize(text1), tokenize(text2))
+  return diff(tokenize(text1), tokenize(text2)).map(addWhiteSpace)
 }
 
 export default diffByWords
