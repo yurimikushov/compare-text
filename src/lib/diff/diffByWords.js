05042c0107b128f6a27e60115881b29882ad3b07
Yuri Mikushov <mikushov.yu@gmail.com>
2022-01-19 09:10:21 +0300

    fix: use white spaces as single words

--- src/lib/diff/diffByWords.js
+++ src/lib/diff/diffByWords.js
@@ -1,7 +1,18 @@
 import diff from './diff'
 
 const tokenize = (text) => {
-  return text.replaceAll('\n', '\n ').split(' ')
+  // Tokenizing can be optimized by combining 'replaceAll()',
+  // 'split()' and 'flatMap()' in one loop
+  return text
+    .replaceAll('\n', '\n ')
+    .split(' ')
+    .flatMap((word) => {
+      if (word.endsWith('\n')) {
+        return word
+      }
+
+      return [word, ' ']
+    })
 }
 
 const diffByWords = (text1, text2) => {
