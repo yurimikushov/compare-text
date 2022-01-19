bb4de49748012616b577800ec813f7b2682d0a46
Yuri Mikushov <mikushov.yu@gmail.com>
2022-01-19 09:08:37 +0300

    Revert "fix: add white spaces afrer diffing"
    This reverts commit 856ea839af756b9823e8ef562ba669d86fe71a44.

--- src/lib/diff/diffByWords.js
+++ src/lib/diff/diffByWords.js
@@ -4,13 +4,8 @@ const tokenize = (text) => {
   return text.replaceAll('\n', '\n ').split(' ')
 }
 
-const addWhiteSpace = ({ value, ...rest }) => ({
-  ...rest,
-  value: value.endsWith('\n') ? value : value + ' ',
-})
-
 const diffByWords = (text1, text2) => {
-  return diff(tokenize(text1), tokenize(text2)).map(addWhiteSpace)
+  return diff(tokenize(text1), tokenize(text2))
 }
 
 export default diffByWords
