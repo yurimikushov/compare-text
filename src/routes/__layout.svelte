4ac1211096ed15c67024cfdfa5110d728598be5f
Yuri Mikushov <mikushov.yu@gmail.com>
2022-01-21 20:28:36 +0300

    chore: init svelte project

---
+++ src/routes/__layout.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+  import '../app.css'
+</script>
+
+<svelte:head>
+  <meta name="description" content="Compare text online" />
+  <title>Compare text</title>
+</svelte:head>
+
+<slot />
---
+++ src/routes/index.svelte
@@ -0,0 +1,5 @@
+<script context="module" lang="ts">
+  export const prerender = true
+</script>
+
+<h1 class="text-9xl text-green-700">Hello world!</h1>
