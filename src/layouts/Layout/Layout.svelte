eff9ba2581f76917e85eccdd17357c4fd7500a13
Yuri Mikushov <mikushov.yu@gmail.com>
2022-01-24 16:40:46 +0300

    feat: add header to layout

--- src/layouts/Layout/Layout.svelte
+++ src/layouts/Layout/Layout.svelte
@@ -3,11 +3,15 @@
   import '../../app.css'
 </script>
 
-<div
-  class={cn(
-    'p-4 md:p-10 min-h-screen',
-    'bg-gradient-to-br from-blue-50 to-white'
-  )}
->
-  <div class="mx-auto max-w-screen-xl w-full h-full"><slot /></div>
+<div class={cn('mx-auto p-4 md:px-10 md:py-6 max-w-screen-xl min-h-screen')}>
+  <header>
+    <div class="text-3xl font-bold">Just compare.</div>
+  </header>
+  <main class="mt-5 mx-auto w-full h-full"><slot /></main>
 </div>
+
+<style>
+  :global(body) {
+    @apply bg-gradient-to-br from-blue-50 to-white;
+  }
+</style>
