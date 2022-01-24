7ea3d8f2504cefa39c39b45b7038cdf760ccc35a
Yuri Mikushov <mikushov.yu@gmail.com>
2022-01-24 17:02:46 +0300

    style: correct viewer ui

--- src/components/DiffViewer/DiffViewer.svelte
+++ src/components/DiffViewer/DiffViewer.svelte
@@ -12,14 +12,14 @@
   class={cn(
     className,
     'table w-full py-2',
-    'rounded-md bg-white border border-gray-200'
+    'rounded-md bg-white border border-gray-300'
   )}
 >
   {#each diff.left as _, i (i)}
     <div class="table-row">
       <LineNumber className="table-cell px-2" number={i + 1} />
       <Line className="table-cell w-1/2 mx-2" line={diff.left[i]} />
-      <LineNumber className="table-cell px-2" number={i + 1} />
+      <LineNumber className="table-cell pl-5 pr-2" number={i + 1} />
       <Line className="table-cell w-1/2" line={diff.right[i]} />
     </div>
   {/each}
