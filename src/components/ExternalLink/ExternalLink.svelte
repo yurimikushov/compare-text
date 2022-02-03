14f284961476aefafaa289a7cf7f4f807d3d0d79
Yuri Mikushov <mikushov.yu@gmail.com>
2022-02-03 19:20:07 +0300

    feat: add external link component

---
+++ src/components/ExternalLink/ExternalLink.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+  import cn from 'classnames'
+
+  export let className = ''
+  export let href: string
+</script>
+
+<a
+  class={cn(className, 'text-blue-700 hover:underline')}
+  {href}
+  target="_blank"
+>
+  <slot />
+</a>
