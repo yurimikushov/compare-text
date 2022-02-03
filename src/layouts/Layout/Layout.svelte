fd07bf06ee0f40def8eacefcab1668e649cd4746
Yuri Mikushov <mikushov.yu@gmail.com>
2022-02-03 19:59:07 +0300

    feat: add nav

--- src/layouts/Layout/Layout.svelte
+++ src/layouts/Layout/Layout.svelte
@@ -1,13 +1,17 @@
 <script lang="ts">
   import cn from 'classnames'
+  import routes from 'constants/routes'
   import '../../app.css'
 </script>
 
 <div class={cn('mx-auto p-4 md:px-10 md:py-6 max-w-screen-xl min-h-screen')}>
-  <header>
-    <div class="text-3xl font-bold">Just compare.</div>
+  <header class="flex justify-between items-center">
+    <a class="text-3xl font-bold" href={routes.home}>Just compare.</a>
+    <nav>
+      <a class="text-gray-500 hover:text-gray-900" href={routes.about}>About</a>
+    </nav>
   </header>
-  <main class="mt-5 mx-auto w-full h-full"><slot /></main>
+  <main class="mt-12 mx-auto w-full h-full"><slot /></main>
 </div>
 
 <style>
