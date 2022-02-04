9b2b0beb265fb922d9c7d21f89f7e0135cd57bab
Yuri Mikushov <mikushov.yu@gmail.com>
2022-02-04 12:11:47 +0300

    ci: add deploy to firebase

--- svelte.config.js
+++ svelte.config.js
@@ -2,8 +2,6 @@ import adapter from '@sveltejs/adapter-static'
 import preprocess from 'svelte-preprocess'
 import path from 'path'
 
-const dev = process.env.NODE_ENV === 'development'
-
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
   // Consult https://github.com/sveltejs/svelte-preprocess
@@ -31,9 +29,6 @@ const config = {
       }
     },
 
-    paths: {
-      base: dev ? '' : '/text-comparator'
-    },
     appDir: 'internal'
   }
 }
